75f424caed84185eb31bdb20ad94a72e14fe3f30
Brendan Geck <brendan@ocelot.net>
2021-02-16 23:27:27 -0500

    Fixing setuptools configuration to allow for distributing with pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name='quant-framework',
     version='0.0.1',
+    author='Brendan Geck',
+    author_email='bpgeck@gmail.com',
+    url='https://github.com/brendangeck/quant-framework',
+    description='A basic framework for backtesting and going live with quant strategies',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=find_packages(),
     scripts=[],
     entry_points={
