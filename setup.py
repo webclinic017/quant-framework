748d4bb0b7c38f41de2d87799e75b9364720c3bf
Brendan Geck <brendan@ocelot.net>
2021-02-18 12:35:21 -0500

    Incrementing package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='quant-framework',
-    version='0.0.1',
+    version='0.0.2',
     author='Brendan Geck',
     author_email='bpgeck@gmail.com',
     url='https://github.com/brendangeck/quant-framework',
